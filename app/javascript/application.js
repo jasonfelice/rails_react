ec0876a92a3dcdf3877db87fd29aa2817b80d50e
Jake Felice <jasonfelice0@gmail.com>
2022-09-27 03:03:18 -0700

    Set up redux store

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,14 @@
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App'
+import store from './redux/configureStore';
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
